0ff6301dd301bd3e624e15004dd5fea1a3259122
Ryan Paroz <ryan.paroz@gmail.com>
2019-12-02 12:05:40 +1000

    Add advent of code 1.1 solution

---
+++ aoc1.1/aoc1.1.hs
@@ -0,0 +1,26 @@
+import Data.List (foldl')
+import Data.List.Split (splitOn)
+import Data.Maybe (maybeToList)
+import Text.Read (readMaybe)
+
+inputFileName :: String
+inputFileName = "aoc1.1.txt"
+
+readValues :: (Integral a, Read a) => FilePath -> IO [a]
+readValues path = (readStrings path) >>= return . foldRead
+            where readStrings :: FilePath -> IO [String]
+                  readStrings path = (readFile path) >>= return . (splitOn "\n")
+                  foldRead :: (Integral a, Read a) => [String] -> [a]
+                  foldRead valuesToRead = foldl' addValue [] valuesToRead
+                        where addValue :: (Integral a, Read a) => [a] -> String -> [a]
+                              addValue acc toRead = (++) acc $ maybeToList $ readMaybe toRead
+
+findFuel :: Integral a => a -> a
+findFuel n = floor (fromIntegral n / 3) - 2
+
+main :: IO ()
+main = do
+    values <- readValues inputFileName
+    putStrLn $ show $ foldl' addMass 0 values
+        where addMass :: Integral a => a -> a -> a
+              addMass acc mass = acc + findFuel mass
